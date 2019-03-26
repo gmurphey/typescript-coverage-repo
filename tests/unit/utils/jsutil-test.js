2a43545939c588e9f85f955b44441756bdc5eb27
Garrett Murphey <gmurphey@gmurphey.com>
2019-03-26 15:26:40 -0400

    WIP

---
+++ tests/unit/utils/jsutil-test.js
@@ -0,0 +1,10 @@
+import jsutil from 'typescript-coverage-repro/utils/jsutil';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | jsutil', function() {
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = jsutil();
+    assert.ok(result);
+  });
+});
---
+++ tests/unit/utils/tsutil-test.ts
@@ -0,0 +1,10 @@
+import tsutil from 'typescript-coverage-repro/utils/tsutil';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | tsutil', function() {
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = tsutil();
+    assert.ok(result);
+  });
+});
